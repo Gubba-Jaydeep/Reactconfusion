c8b842158b5c9d390aecbc80edf7ecd7bbbb4233
Gubba-Jaydeep <jaydeepgubba@gmail.com>
2020-09-06 20:08:08 +0530

    Redux Thunk

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,28 +1,49 @@
 import React from 'react';
 import { Card, CardImg, CardImgOverlay, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
 import { Link } from 'react-router-dom';
-    function RenderMenuItem({ dish, onClick}){
-        return(
-            <Card>
-                <Link to={`/menu/${dish.id}`}>
-                <CardImg width="100%" src={dish.image} alt={dish.name}/>    
+import { Loading } from './LoadingComponent';
+function RenderMenuItem({ dish, onClick }) {
+    return (
+        <Card>
+            <Link to={`/menu/${dish.id}`}>
+                <CardImg width="100%" src={dish.image} alt={dish.name} />
                 <CardImgOverlay>
                     <CardTitle>{dish.name}</CardTitle>
                 </CardImgOverlay>
-                </Link>
-            </Card>
+            </Link>
+        </Card>
+    );
+}
+const Menu = (props) => {
+    const menu = props.dishes.dishes.map((dish) => {
+        return (
+            <div key={dish.id} className="col-12 col-md-5 m-1">
+                <RenderMenuItem dish={dish} />
+            </div>
+        );
+    });
+    if (props.dishes.isLoading) {
+        return(
+            <div className="container">
+                <div className="row">            
+                    <Loading />
+                </div>
+            </div>
         );
     }
-    const Menu = (props) =>{
-        const menu = props.dishes.map((dish) => {
-            return(
-                <div key={dish.id} className="col-12 col-md-5 m-1">
-                    <RenderMenuItem dish={dish}/>
-                </div>
-            );
-        });
-
+    else if (props.dishes.errMess) {
         return(
+            <div className="container">
+                <div className="row"> 
+                    <div className="col-12">
+                        <h4>{props.dishes.errMess}</h4>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+    else
+        return (
             <div className="container">
                 <div className='row'>
                     <Breadcrumb>
@@ -40,8 +61,8 @@ import { Link } from 'react-router-dom';
             </div>
 
         );
-    }
-        
+}
+
 
 
 export default Menu;
